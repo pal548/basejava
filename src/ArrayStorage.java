e0d7067d3ed90f1435fcb38947a327ba7698ade7
Лукьянский П.А <affklay@gmail.com>
2018-03-05 01:28:40 +0300

    added check for resume is already in storage in save() method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,24 +6,21 @@ import java.util.Arrays;
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     private int size = 0;
+
     void clear() {
-        for(int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
-    private int find(String uuid) {
-        for(int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     void save(Resume r) {
-        if(size < storage.length) {
+        if (size < storage.length) {
+            int i = find(r.uuid);
+            if (i > -1) {
+                System.out.printf("Save error: resume is already in storage");
+                return;
+            }
             storage[size] = r;
             size++;
         } else {
@@ -47,7 +44,7 @@ public class ArrayStorage {
         if (i > -1) {
             // на место удаляемого переносим последний элемент
             size--;
-            if(i < size) {
+            if (i < size) {
                 storage[i] = storage[size];
             }
             storage[size] = null;
@@ -69,10 +66,19 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage,size);
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
         return size;
+    }
+
+    private int find(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
