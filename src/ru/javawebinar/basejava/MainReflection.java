83a2be42921df8780b4f6bbdff229fbaac80e3c7
Лукьянский П.А <LukianskiiPA@alfastrah.ru>
2018-03-13 15:15:03 +0300

    added reflection, made invocation of toString() via reflection

---
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.basejava;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume r = new Resume();
+        Field f = r.getClass().getDeclaredFields()[0];
+        f.setAccessible(true);
+        System.out.println(f.getName());
+        System.out.println(f.get(r));
+        f.set(r, "new_uuid");
+        Method m = r.getClass().getMethod("toString");
+        System.out.println(m.invoke(r));
+    }
+}
