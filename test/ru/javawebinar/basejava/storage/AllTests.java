2331623ece24aa80ea4f796ec8581e01e1a88573
Лукьянский П.А <affklay@gmail.com>
2018-05-16 23:34:07 +0300

    added all test suite class

---
+++ test/ru/javawebinar/basejava/storage/AllTests.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                ArrayStorageTest.class,
+                SortedArrayStorageTest.class,
+                ListStorageTest.class,
+                MapUuidStorageTest.class,
+                MapIntStorageTest.class,
+                MapResumeStorageTest.class,
+                ObjectStreamFileStorageTest.class,
+                ObjectStreamPathStorageTest.class
+        }
+)
+public class AllTests {
+}
