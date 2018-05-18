f4990cff838c753b2cba79a6e5ae364fe1e975a4
Лукьянский П.А <LukianskiiPA@alfastrah.ru>
2018-05-18 20:40:26 +0300

    moved serializers to separate package

--- src/ru/javawebinar/basejava/storage/serializers/ObjectStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializers/ObjectStreamSerializer.java
@@ -1,4 +1,4 @@
-package ru.javawebinar.basejava.storage;
+package ru.javawebinar.basejava.storage.serializers;
 
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
--- test/ru/javawebinar/basejava/storage/ObjectStreamPathStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ObjectStreamPathStorageTest.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.basejava.storage;
 
+import ru.javawebinar.basejava.storage.serializers.ObjectStreamSerializer;
+
 public class ObjectStreamPathStorageTest extends AbstractStorageTest {
     public ObjectStreamPathStorageTest() {
         super(new PathStorage(STORAGE_DIR, new ObjectStreamSerializer()));
